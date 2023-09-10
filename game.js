2c1a1d8fa23c43116ac0e938a612ddf8677a6343
ganjela <nikaganjelashvili@gmail.com>
2023-09-10 14:09:17 +0400

    modified game and playRound functions, also deleted capitilize

--- game.js
+++ game.js
@@ -2,13 +2,6 @@ function getRandomInt(max){
     return Math.floor(Math.random()*max);
 }
 
-function capitalize(str){
-    str = str.toLowerCase();
-    
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-
 function getComputerChoice(){
     let choices  = ["Rock","Paper","Scissors"];
     let randomIndex = getRandomInt(3);
@@ -17,46 +10,35 @@ function getComputerChoice(){
 }
 
 function playRound(playerSelection, computerSelection){
-    let result = "";
+    let text = "";
 
     if(playerSelection === computerSelection){
-        result = "It's a Tie";
+        text = "It's a Tie";
     }else if(playerSelection==="Rock" && computerSelection==="Paper"){
-        result = "You Lose! Paper beats Rock";
+        text = "You Lose! Paper beats Rock";
     }else if(playerSelection==="Rock" && computerSelection==="Scissors"){
-        result = "You Win! Rock beats Scissors";
+        text = "You Win! Rock beats Scissors";
     }else if(playerSelection=="Paper" && computerSelection==="Rock"){
-        result = "You Win! Paper beats Rock";
+        text = "You Win! Paper beats Rock";
     }else if(playerSelection=="Paper" && computerSelection==="Scissors"){
-        result = "You Lose! Scissors beats Paper";
+        text = "You Lose! Scissors beats Paper";
     }else if(playerSelection=="Scissors" && computerSelection==="Rock"){
-        result = "You Lose! Rock beats Scissors";
+        text = "You Lose! Rock beats Scissors";
     }else if(playerSelection=="Scissors" && computerSelection==="Paper"){
-        result = "You Win! Scissors beats Paper";
-    }else{
-        result = "Invalid Input";
+        text = "You Win! Scissors beats Paper";
     }
-    
-    return result;
+
+    const result = document.querySelector('.result');
+
+    result.textContent = text;
 }
 
 function game(){
-    let playerSelection = "";
-    let computerSelection = "";
-
-    for(i=1;i<=5;i++){
-        do{
-            let selection = prompt("Rock, Paper, Scissors? :");
-            playerSelection = capitalize(selection);
-        }
-        while(playerSelection!=="Rock" && playerSelection!=="Paper" && playerSelection!="Scissors");
-
-        computerSelection = getComputerChoice();
-        console.log("Player Selected: " + playerSelection);
-        console.log("Computer Selected: " + computerSelection);
-
-        console.log(`Round ${i}: ` + playRound(playerSelection,computerSelection));
-    }
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => button.addEventListener('click',()=>{
+        playRound(button.textContent,getComputerChoice());
+    }));    
 }
 
 game();
+
