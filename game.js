1306460ce1a294b349b11917dd9e768aaf4c914f
ganjela <nikaganjelashvili@gmail.com>
2023-09-10 15:26:27 +0400

    nested game and playRound functions and added score tracker

--- game.js
+++ game.js
@@ -9,35 +9,64 @@ function getComputerChoice(){
     return choices[randomIndex];
 }
 
-function playRound(playerSelection, computerSelection){
-    let text = "";
-
-    if(playerSelection === computerSelection){
-        text = "It's a Tie";
-    }else if(playerSelection==="Rock" && computerSelection==="Paper"){
-        text = "You Lose! Paper beats Rock";
-    }else if(playerSelection==="Rock" && computerSelection==="Scissors"){
-        text = "You Win! Rock beats Scissors";
-    }else if(playerSelection=="Paper" && computerSelection==="Rock"){
-        text = "You Win! Paper beats Rock";
-    }else if(playerSelection=="Paper" && computerSelection==="Scissors"){
-        text = "You Lose! Scissors beats Paper";
-    }else if(playerSelection=="Scissors" && computerSelection==="Rock"){
-        text = "You Lose! Rock beats Scissors";
-    }else if(playerSelection=="Scissors" && computerSelection==="Paper"){
-        text = "You Win! Scissors beats Paper";
-    }
-
-    const result = document.querySelector('.result');
-
-    result.textContent = text;
-}
-
 function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+    let end = false;
+
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => button.addEventListener('click',()=>{
-        playRound(button.textContent,getComputerChoice());
-    }));    
+        if(!end){
+            playRound(button.textContent,getComputerChoice());
+        }
+    }));
+    
+    function playRound(playerSelection, computerSelection){
+        let text = "";
+    
+        if(playerSelection === computerSelection){
+            text = "It's a Tie";
+        }else if(playerSelection==="Rock" && computerSelection==="Paper"){
+            text = "You LOST The Round! Paper beats Rock";
+            computerScore++;
+        }else if(playerSelection==="Rock" && computerSelection==="Scissors"){
+            text = "You WON The Round! Rock beats Scissors";
+            playerScore++;
+        }else if(playerSelection=="Paper" && computerSelection==="Rock"){
+            text = "You WON The Round! Paper beats Rock";
+            playerScore++;
+        }else if(playerSelection=="Paper" && computerSelection==="Scissors"){
+            text = "You LOST The Round! Scissors beats Paper";
+            computerScore++;
+        }else if(playerSelection=="Scissors" && computerSelection==="Rock"){
+            text = "You LOST The Round! Rock beats Scissors";
+            computerScore++;
+        }else if(playerSelection=="Scissors" && computerSelection==="Paper"){
+            text = "You WON The Round! Scissors beats Paper";
+            playerScore++;
+        }
+
+        const result = document.querySelector('.result');
+        const score = document.querySelector('.score');
+        score.textContent = `Player: ${playerScore} Computer: ${computerScore}`;
+
+        if(playerScore==5){
+            result.textContent = "CONGRATS! YOU WON THE GAME!";
+            end = true;
+            return;
+        }else if(computerScore==5){
+            end = true;
+            result.textContent = "YOU LOST THE GAME. TRY AGAIN!";
+            return;
+        }else{
+            result.textContent = text;
+        }
+
+        
+    
+
+    }
+
 }
 
 game();
